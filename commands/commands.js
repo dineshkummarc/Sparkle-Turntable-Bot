b9c7d1ab748134405b40be3cb8ac9dad9a577cb1
Vineet Kumar <vineet@doorstop.net>
2012-04-27 14:56:44 -0400

    Alternative formatting for commands.

--- commands/commands.js
+++ commands/commands.js
@@ -1,12 +1,13 @@
 //Displays the list of commands found in ./commands/
-
-exports.name = 'commands';
-exports.hidden = false;
-exports.enabled = true;
-exports.matchStart = false;
-exports.handler = function(data) {
-    var response = 'Commands: ' + commands.filter(function(command) {
-        return command.enabled && !command.hidden;
-    }).map(function(command){return command.name;}).sort().join(', ');
-    output({text: response, destination: data.source, userid: data.userid});
+module.exports = {
+    name = 'commands',
+    hidden = false,
+    enabled = true,
+    matchStart = false,
+    handler = function(data) {
+        var response = 'Commands: ' + commands.filter(function(command) {
+            return command.enabled && !command.hidden;
+        }).map(function(command){return command.name;}).sort().join(', ');
+        output({text: response, destination: data.source, userid: data.userid});
+    }
 }
